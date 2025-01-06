999a27eb7f024e1474d5903ff65ed47dcdab6b09
Anupam <anupam.kaul@yahoo.com>
2025-01-06 14:59:13 -0800

    Add code to remove extraneous whitespaces and combine-split all tokens we don't care about

--- tokenizers/example-tokensplit.py
+++ tokenizers/example-tokensplit.py
@@ -3,6 +3,7 @@ text = "Hello, world! This, really this, is a big long test. Let's see how this 
 print(text)
 
 # the resulting array contains the split token as well
+# result is an array of strings thus separated by comma elements when printed out
 
 # split text on white space
 result = re.split(r'(\s)', text)
@@ -18,3 +19,17 @@ print(result)
 result = re.split(r'(\!)', text)
 print("\nsplit on exclamation mark")
 print(result)
+
+# combining all of them (use OR and keep each in a set)
+result = re.split(r'([\!]|[\s]|[\,])', text)
+print("\nsplit on all")
+print(result)
+
+# remove the extra whitespaces
+# strip call removes leading and trailing whitespaces, 
+# so if applying strip (removing whitespace) it still makes a string exist, add it back to result array
+result = [item for item in result if item.strip()]
+print("\nremove extra whitespaces that existed by themselves")
+print (result)
+
+
