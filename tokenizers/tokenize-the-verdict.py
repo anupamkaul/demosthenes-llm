dcad8526948542eeaf8c3f99bac168934bc63f90
Anupam <anupam.kaul@yahoo.com>
2025-01-07 11:15:34 -0800

    Remove white spaces from tokenized "the verdict". Reduces memory by half

--- tokenizers/tokenize-the-verdict.py
+++ tokenizers/tokenize-the-verdict.py
@@ -3,5 +3,6 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
 
 import re
 preprocessed = re.split(r'( [,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item for item in preprocessed if item.strip()] # remove extra white spaces (optional)
 print(len(preprocessed))
 print(preprocessed)
