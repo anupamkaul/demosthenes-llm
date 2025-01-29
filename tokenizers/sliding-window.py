2b3b709e40d69a1a7b4d50f38f912e3720c7f9c2
Anupam <anupam.kaul@yahoo.com>
2025-01-28 23:27:14 -0800

    tokenizers % python sliding-window.py 5145 x:  [290, 4920, 2241, 287] y:       [4920, 2241, 287, 257] [290] ----> 4920 [290, 4920] ----> 2241 [290, 4920, 2241] ----> 287 [290, 4920, 2241, 287] ----> 257  and ---->  established  and established ---->  himself  and established himself ---->  in  and established himself in ---->  a

--- tokenizers/sliding-window.py
+++ tokenizers/sliding-window.py
@@ -1,3 +1,12 @@
+'''
+Given a text sample, extract input blockers as subsamples
+that serve as input to the LLM. The LLM's prediction task
+during training is to predict the next word that follows the
+input block. During training, we mask out all workds that are
+past the target. The text first undergoes tokenization
+'''
+
+
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
@@ -8,6 +17,34 @@ tokenizer = tiktoken.get_encoding("gpt2")
 enc_text = tokenizer.encode(raw_text)
 print(len(enc_text))
 
+'''
 import time
 time.sleep(2)
 print(enc_text)
+'''
+
+# remove the first 50 tokens for demo purposes / more interesting samples
+enc_sample = enc_text[50:]
+
+# simplest input-target pairing : x and y
+# x is inputs-token and y is targets (here inputs shifted by 1)
+
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+
+print(f"x:  {x}")
+print(f"y:       {y}")
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(context, "---->", desired)
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+
+
