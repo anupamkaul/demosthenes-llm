2c42472ec91a759e994af885554eb52dd55acfc0
Anupam <anupam.kaul@yahoo.com>
2025-01-28 21:26:32 -0800

    Now make it Akwirw ier
    [33901, 86, 343, 86, 220, 959]
    
    shows the tokens split into Ak w ir "" ier

--- tokenizers/tiktoken-usage.py
+++ tokenizers/tiktoken-usage.py
@@ -28,7 +28,7 @@ print(strings)
 
 # can see that endoftext is assigned id 50256. Total vocab is 50257
 
-text = "Akwiewier"
+text = "Akwirw ier"
 
 print("\nencoding unknown words (Akwiewier)..\n")
 integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
