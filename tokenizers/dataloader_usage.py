cc19e6ff6f66343c09f339c5d2ce7a8cac1cc0c3
Anupam <anupam.kaul@yahoo.com>
2025-02-04 18:38:49 -0800

    Add positional embeddings (also modularize dataloader for reuse)
    python positional_embeddings.py
    Embedding(50257, 256)
    Parameter containing:
    tensor([[ 0.8462, -0.2144, -0.0641,  ..., -0.7157, -0.9633,  0.6325],
            [ 0.1313, -1.4195, -1.1869,  ..., -2.6984, -1.5023,  0.5386],
            [ 0.4793,  0.2323,  0.7662,  ...,  1.0722,  1.4306,  0.1038],
            ...,
            [-0.1845,  0.7449, -2.0165,  ..., -0.0450, -0.1668, -1.5906],
            [ 0.5488,  0.5713,  0.1501,  ..., -0.4821,  0.2603,  0.6500],
            [ 1.1804, -0.5621,  0.2469,  ...,  0.0925, -0.2421, -0.9201]],
           requires_grad=True)
    Token IDs:
     tensor([[   40,   367,  2885,  1464],
            [ 1807,  3619,   402,   271],
            [10899,  2138,   257,  7026],
            [15632,   438,  2016,   257],
            [  922,  5891,  1576,   438],
            [  568,   340,   373,   645],
            [ 1049,  5975,   284,   502],
            [  284,  3285,   326,    11]])
    
    Inputs shape:
     torch.Size([8, 4])
    torch.Size([8, 4, 256])
    torch.Size([4, 256])
    torch.Size([8, 4, 256])

--- tokenizers/dataloader_usage.py
+++ tokenizers/dataloader_usage.py
@@ -5,37 +5,21 @@ Pytorch's DataLoader (this encompasses the DatasetV1 class above)
 
 '''
 
-import GPTDatasetV1
-from torch.utils.data import Dataset, DataLoader
-import tiktoken
-
-def create_dataloader_v1(txt, batch_size = 4, max_length = 256, 
-                         stride = 128, shuffle=True, drop_last=True,
-                         num_workers=0):
-
-   tokenizer = tiktoken.get_encoding("gpt2")
-   dataset = GPTDatasetV1.GPTDatasetV1(txt, tokenizer, max_length, stride)
-   dataloader = DataLoader(
-       dataset,
-       batch_size=batch_size,
-       shuffle=shuffle,
-       drop_last=drop_last,
-       num_workers=num_workers
-   )
-
-   return dataloader
-
+import dataloaderV1
 
 '''
-Test the above code. Let's use the dataloader with a batch size of 1
+Test the dataloader code (based on GPTDatasetV1) . 
+
+Let's use the dataloader with a batch size of 1
 for an LLM with a context size of 4 to develop an intuition of how the
 GPTDatasetV1 class and the create_dataloader functions work together
+
 '''
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+dataloader = dataloaderV1.create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
 
 # now we convert dataloader into a Python iterator to fetch
 # the next entry via Python's built-in next() function
@@ -49,7 +33,7 @@ second_batch = next(data_iter)
 print("batch 2: ", second_batch)
 
 # let's increase the batch size to 8 and stride to 4
-dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
+dataloader = dataloaderV1.create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
 
 data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
--- tokenizers/positional_embeddings.py
+++ tokenizers/positional_embeddings.py
@@ -1,17 +1,19 @@
 '''
 
-Previously we saw how embeddings can work (embeddings_from_token.py)
+Previously we saw how embeddings can work (file "embedding_from_token.py")
 LLMs' self-attention mechanisms don't have a notion of the position or
 order of the tokens within a sequence. (The same token ID always gets mapped
 to the same vector representation). So we need to inject additional position
 information into the LLM
 
 Two categories of position-aware embeddings:
+
 a. relative positional embeddings
 b. absolute positional embeddings
 
 For each position in the input sequence, a unique embedding is added to the 
 token's embedding to convey its exact location (absolute position embedding).
+
 In relative instead of focusing on absolute position, the model learns the relationships
 in terms of "how far apart" rather than "at which exact position".
 
@@ -34,7 +36,9 @@ import torch
 
 vocab_size = 50257
 output_dim = 256
+
 token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
 print(token_embedding_layer)
 print(token_embedding_layer.weight)
 
@@ -45,15 +49,70 @@ we embed each token in each batch into a 256-dim vector. If we have a batch
 of 8 with 4 tokens each, the result would be an 8 * 4 * 256 tensor.
 
 '''
-import GPTDatasetV1
+import dataloaderV1
 
-print("\nback to the program here..\n")
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
 
 max_length = 4
+dataloader = dataloaderV1.create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length,
+    stride=max_length, shuffle=False
+)
 
+data_iter = iter(dataloader)
+inputs, target = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
 
+'''
+We can see that token ID tensor is 8 * 4 dim, meaning that the data
+batch consists of eight text samples with 4 tokens each
 
+Let us now use the embedding layer to embed these token IDs into 256-dim
+vectors:
+'''
 
+token_embeddings = token_embedding_layer(inputs)
+print(token_embeddings.shape)  # returns torch.Size([8, 4, 256])
+# print("token embeddings: \n", token_embeddings)
 
+'''
+Tghe 8 * 4 * 256 dim vector tensor output shows that each token ID is 
+now embedded as a 256 dimensional vector.
 
+For a GPT model's absolute embedding approach, we jsut need to create
+another embedding layer that has the same embedding dim as the 
+token_embedding_layer
 
+'''
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer( torch.arange(context_length) )
+print(pos_embeddings.shape)
+
+'''
+The input to pos_embeddings is a placeholder vector usually torch.arange(context_length).
+This contains a sequence of numbers 0,1,... up to the maximum input_length-1.
+The context length is a variable that represents the supported input size of the LLM.
+Here it is equal to maximum length of input text. In practice if it is longer, we truncate
+the text.
+
+The output is torch.Size([4, 256]) meaning the positional embedding tensor consists of 
+four 256 dim vectors. 
+
+We now add these directly to token embeddings (as promised) where PyTorch will add the
+4 * 256-dim pos_embeddings tensor to each of the 4 * 256-dim token embedding tensor in
+each of the 8 batches:
+'''
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
+
+'''
+This returns torch.Size([8, 4, 256]).
+
+The input_embeddings that we have created, are the input embedded examples that can
+now be processed by the main LLM modules.
+'''
