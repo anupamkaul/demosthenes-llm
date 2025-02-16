Fix a bug in attention scores calculation
If input is X rows then attention score of a query has to have X dimensions also,
as every element of input is dot-productized with the query (at an elemental level).

Thus if input has 6 rows then the attention score of a query (no matter how many dims it is represented with)
will also have 6 elements to it, with every element representing a "relation" between that query (input token)
and every other input token of that sentence. (for the LLM to get more understanding)

Of course the relationship calculation will later be based on trainable weights

attention/dot_product.py
@@ -44,9 +44,9 @@ print("calculate attn_scores of input token 2:")
 
 for i,x_i in enumerate(inputs):
     print("i=", i, " x_i = ", x_i)
-    attn_scores_2 = torch.dot(x_i, query)
+    attn_scores_2[i] = torch.dot(x_i, query)
 
-print("\nattn score of query element 2 is the dot product of itself with each input token : ", attn_scores_2, "\n")
+print("\nattn score of query element 2 is the dot product of itself with each input token shown below : \n", attn_scores_2, "\n")
 
 # here is how I validate what exactly does a dot product do:
 # see if my hypothesis for a dot product's result matches tensor.dot's definition