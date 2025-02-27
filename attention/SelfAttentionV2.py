c3478d61cb363b3fa3a1fdeb46360ab58a608c2a
Anupam <anupam.kaul@yahoo.com>
2025-02-27 06:34:54 -0800

    Implement V2 of Self Attention based on nn.Linear which contains better init and better matmul capabilities Test the code with a test_SelfAttentionV2.py
    python test_SelfAttentionV2.py
    tensor([[-0.0739,  0.0713],
            [-0.0748,  0.0703],
            [-0.0749,  0.0702],
            [-0.0760,  0.0685],
            [-0.0763,  0.0679],
            [-0.0754,  0.0693]], grad_fn=<MmBackward0>)

---
+++ attention/SelfAttentionV2.py
@@ -0,0 +1,34 @@
+'''
+Implementing a compact self-attention python class (following self_attention_trainable_wts.py)
+
+Self-attention involves trainable wt matrices Wq, Wk, Wv. These matrices transform input data into queries,
+keys and values (following a general database search mechanism). As the model is exposed to more data during training,
+it adjusts these trainable wts (as we will see later)
+
+We can improve SelfAttention_v1 impl by utilizing PyTorch's nn.Linear layers, which effectively perform matrix multiplication
+when the bias units are disabled. Additionally, a signf adv of using nn.Linear instead of manually impl (torch.rand(...)) is that
+nn.Linear has an optimized wt initialization scheme, contrubuting to more stasble and effective model training.
+'''
+
+import torch
+import torch.nn as nn
+
+class SelfAttention_v2(nn.Module):
+
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+
+        self.W_query = nn.Linear(d_in, d_out, bias = qkv_bias) # use Linear instead of Parameters for better optimized inits
+        self.W_key   = nn.Linear(d_in, d_out, bias = qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias = qkv_bias)
+
+    def forward(self, x):
+        keys =    self.W_key(x)    # use better optimized vn of matmul (see V1)
+        queries = self.W_query(x)  # use better optimized vn of matmul (see V1)`
+        values =  self.W_value(x)  # use better optimized vn of matmul (see V1)
+
+        attn_scores  = queries @ keys.T # omega
+        attn_weights = torch.softmax( attn_scores / keys.shape[-1]**0.5, dim=-1 ) # scaled dot product to avoid vanishing gradients
+
+        context_vec = attn_weights @ values
+        return context_vec 
