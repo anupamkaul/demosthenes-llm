cf1d83787dc9d9c1d7e936fafd14ebc6234ecb4d
Anupam <anupam.kaul@yahoo.com>
2025-03-07 10:43:57 -0800

    Add test for Multiheaded Attention (MHA). Works. Fix a bug in MHA class (d_out was d_out- resulting in negative tensor allocation error). Can see multiple stacked outputs in the associated dump.txt and can see the combined context_vector matrix now with its shape.

--- attention/MultiHeadAttn_StackedCausalSA.py
+++ attention/MultiHeadAttn_StackedCausalSA.py
@@ -4,10 +4,11 @@ import SelfAttentionV2Causal as SelfAttn
 
 '''
 
-Exted causal attention class over multiple heads (stacked up causal self-attention units). This is multi-head attention.
+Extend causal attention class over multiple heads (stacked up causal self-attention units). This is multi-head attention.
 The term 'multi-head' refers to the same input stream but dividing the attention mechanisms into multiple "heads" with each
 head operating independently. In this context, a single causal attention module can be considered single-head attention, where
-there is only 1 set of attention weights processing the input sequentially.
+there is only 1 set of attention weights processing the input sequentially. (Heads here refers to multiple units stacked on top of
+each other. These are the same functional units, like Wt Matrices, Q/K/V matrices, and the Context Vector outputs.
 
 First, we will intuitively build a multi-head attention module by stacking multiple Causal-Attention modules (this file). We use
 nn.ModuleList to help us with the class stacking.
@@ -15,9 +16,11 @@ nn.ModuleList to help us with the class stacking.
 Next, we will implement the same multi-=head attention in a slightly more complicated but more computationally efficient way.
 
 Input X remains the same. Instead of 1 Wq Wk and Wv now we have multiple. Similarly there are multiple Qx Kx and Vx and multiple 
-context vectors that get generated. (Its like input is the same but everything else is directly stacked up in Z-order). E.g. if there
-were 2 Causal SA modules, we now have multi-headed attention with 2 modules. Two value weight matrices for ex. Wv1 and Wv2. Same applies
-to other Wt matrices, Wq and Wk. We obtain 2 sets of context vectors Z1 and Z2 that we can finally combine into a single context vector matrix Z.
+context vectors that get generated. (Its like input is the same but everything else is directly stacked up in Z-order). 
+
+E.g. if there were 2 Causal SA modules, we now have multi-headed attention with 2 modules. Two value weight matrices for ex. Wv1 and Wv2. 
+Same applies to other Wt matrices, Wq and Wk.
+We obtain 2 sets of context vectors Z1 and Z2 that we can finally combine into a single context vector matrix Z.
 
 Remember this is still infra/shell, (the wts haven't been trained yet and so wt matrices are default/random). We run the attention mechanism
 multiple times (in parallel) with different, learned linear projections - the results of multiplying the input data (like query, key and value
@@ -37,9 +40,7 @@ class MultiHeadAttentionWrapper(nn.Module):
 
         self.heads = nn.ModuleList(
 
-            [SelfAttn.SelfAttentionV2Causal(
-                d_in, d_out- context_length, dropout, qkv_bias
-            )
+            [SelfAttn.SelfAttentionV2Causal( d_in, d_out, context_length, dropout, qkv_bias)
             for _ in range(num_heads)]
         )
 
