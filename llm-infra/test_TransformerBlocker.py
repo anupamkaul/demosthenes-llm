8571c2d4531f2c65e08d3879f2524112a37236c4
Anupam <anupam.kaul@yahoo.com>
2025-04-08 10:55:15 -0700

    Add notes to the implications of transformer block's same size output dims. While it makes it useful for sequence to sequence transformations, the output actually encodes contextual info from the entire input sequence that passed before it
    This is thanks to attention where the qkv vectors are trained separately to correctly
    associate context within tokens, and where its flow and weight connections now contain
    contextual info as represented within tensors. The transformer block enables the contextual
    information to flow through by combining a feed forward layer with gradient optimizations to GELU
    via layer norm, dropout and shortcuts, and provides the basic block for GPT architecture

--- llm-infra/test_TransformerBlocker.py
+++ llm-infra/test_TransformerBlocker.py
@@ -12,6 +12,23 @@ print("Transformer: Input Shape: ", x.shape, "\n")
 print("Transformer: Output Shape: ", output.shape, "\n")
 print("Transformer Output: \n", output)
 
+'''
+
+As we can see from dump_TransformBlock.txt, the transformer block maintains the input dimensions in its output,
+indicating that the transformer architecture processes sequences of data without altering their shape throughout the network.
+
+The preservation of shape throughout the transformer block architecture is not incidental but a crucial aspect of its design. 
+This design enables its effective application across a wide range of sequence-to-sequence tasks, where each output vector directly 
+corresponds to an input vector, maintaining a one-to-one relationship. However, the output is a context vector that encapsulates information 
+from the entire input sequence. This means that while the physical dimensions of the sequence (length and feature size) remain unchanged as 
+it passes through the transformer block, the content of each output vector is re-encoded to integrate contextual information from across the
+entire input sequence.
+
+With the transformer block implemented, we now have all the building blocks needed to implement the GPT architecture.The transformer block 
+combines layer normalization, the feed forward network, GELU activations, and shortcut connections. As we will eventually see, this transformer 
+block will make up the main component of the GPT architecture.
+
+'''
 
 
 
