5ce6ee21eacd2e43288c53f897c67865a22129d8
Anupam <anupam.kaul@yahoo.com>
2025-04-10 15:00:23 -0700

    Analyze and print number of params (wts) of the FeedForward class used in the transformer block
    Comes to roughly 4M 30K params

--- llm-infra/test_FeedForward.py
+++ llm-infra/test_FeedForward.py
@@ -10,3 +10,17 @@ out = ffn(x)
 print("out shape: \n", out.shape)
 print("out: \n", out)
 
+'''
+Model analysis : let's understand how many parameters (wts) are in this 
+feed fwd class
+'''
+
+params_ff = sum(p.numel() for p in ffn.parameters()) 
+print(f"params of the feedforward class: {params_ff:,}")
+
+'''
+params of the feedforward class: 4,722,432
+
+(this is 6149 times 768 (emb_dim)) 
+
+'''
