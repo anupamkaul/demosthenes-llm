b60c580f3b6cb59f8803ce8f470954baf4d2f3b1
Anupam <anupam.kaul@yahoo.com>
2025-04-10 10:12:42 -0700

    Analyze size of GPT-2 model. Should be 124M but is 163M because of weight tying (out_layer reuses token_emb weights. This is proved by removing out out_layer and checking that weights go back to 124M)

--- llm-infra/test_GPTModel.py
+++ llm-infra/test_GPTModel.py
@@ -40,3 +40,74 @@ logits = model(batch)
 
 print("Output shape: ", logits.shape)
 print(logits)
+
+'''
+Output shape:  torch.Size([2, 4, 50257])
+tensor([[[ 0.1381,  0.0077, -0.1963,  ..., -0.0222, -0.1060,  0.1717],
+         [ 0.3865, -0.8408, -0.6564,  ..., -0.5163,  0.2369, -0.3357],
+         [ 0.6989, -0.1829, -0.1631,  ...,  0.1472, -0.6504, -0.0056],
+         [-0.4290,  0.1669, -0.1258,  ...,  1.1579,  0.5303, -0.5549]],
+
+        [[ 0.1094, -0.2894, -0.1467,  ..., -0.0557,  0.2911, -0.2824],
+         [ 0.0882, -0.3552, -0.3527,  ...,  1.2930,  0.0053,  0.1898],
+         [ 0.6091,  0.4702, -0.4094,  ...,  0.7688,  0.3787, -0.1974],
+         [-0.0612, -0.0737,  0.4751,  ...,  1.2463, -0.3834,  0.0609]]],
+       grad_fn=<UnsafeViewBackward0>)
+
+The shape of the output (logits) of the GPT shows the vocab size of the tokenizer.
+Next we will convert the output back into text to get the context back in human form.
+
+Let's now analyze the size of the model. This would be very useful for EdgeAI scenarios.
+
+We can use the numel (number of elements) method to collect total number of params (wts)
+in the model's parameter tensors so lets start with that
+
+'''
+
+total_params = sum(p.numel() for p in model.parameters()) # we need to sum numel always..
+print(f"Total number of parameters: {total_params:,}")
+
+'''
+Total number of parameters: 163,009,536 
+
+This is greater than 124M that we started out with. Why?
+This is related to "Weight Tying" which was used with GPT2.
+Basically GPT2 re-uses the wts from the token embedding layer in its output layer.
+
+'''
+
+print("Token embedding layer shape:", model.tok_emb.weight.shape)
+print("Output layer shape:", model.out_head.weight.shape)
+
+'''
+Token embedding layer shape: torch.Size([50257, 768])
+Output layer shape: torch.Size([50257, 768])
+
+To test the weight typing theory let's remove weights of the output_layer (out_head)
+from the total params and see what we land with:
+'''
+
+total_params_gpt2 = {
+    total_params - sum(p.numel() for p in model.out_head.parameters())
+}
+
+print("Number of trainable parameters considering weight tying: ", total_params_gpt2)
+
+'''
+debug: not printing out total_params_gpt2 as an f-string:
+
+# for better formatting:
+print(f"Total number of parameters: {total_params:,}")
+print(f"Total number of parameters: {total_params_gpt2:,}")
+
+print(f"Number of trainable parameters "
+      f"considering weight tying: {total_params_gpt2:,}"
+      )
+
+In Python, the print(f"...") syntax, available from Python 3.6 onwards, utilizes f-strings (formatted string literals) 
+to embed expressions inside string literals for formatting. The f before the opening quote signifies that it's an f-string. 
+Inside the string, expressions enclosed in curly braces {} are evaluated and their values are inserted into the string.
+'''
+
+
+
