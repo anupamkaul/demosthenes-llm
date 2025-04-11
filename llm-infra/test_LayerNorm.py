7cf5a1bf103265eb430698aae3af0b6fa5320170
Anupam <anupam.kaul@yahoo.com>
2025-04-10 18:37:30 -0700

    Change LayerNorm test to scale emb_dim from 5 to 768 (reasonable for GPTsmall) Calculate the parameters (which is double based on the scale and shift blocks)

--- llm-infra/test_LayerNorm.py
+++ llm-infra/test_LayerNorm.py
@@ -15,11 +15,13 @@ batch_example = torch.stack(batch_example, dim=0)
 '''
 
 torch.manual_seed(123)
-batch_example = torch.randn(2, 5)
+#batch_example = torch.randn(2, 5)
+batch_example = torch.randn(2, 768)
 
 print("batch example: \n", batch_example)
 
-ln = _ln.LayerNorm(emb_dim = 5)
+#ln = _ln.LayerNorm(emb_dim = 5)
+ln = _ln.LayerNorm(emb_dim = 768)
 out_ln = ln(batch_example)
 print("out layer normalized:\n", out_ln)
 
@@ -27,3 +29,10 @@ mean = out_ln.mean(dim=-1, keepdim=True)
 var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 print("Mean:\n", mean)
 print("Variance:\n", var)
+
+'''
+Parametric analysis of LayerNorm
+'''
+
+params_lnm = sum(p.numel() for p in ln.parameters()) 
+print(f"params of the LayerNorm class: {params_lnm:,}")
