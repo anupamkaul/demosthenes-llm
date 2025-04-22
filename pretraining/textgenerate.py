8bb3e3f32acde54f013cda0c2f5c643d29fbc8bc
Anupam <anupam.kaul@yahoo.com>
2025-04-22 16:52:15 -0700

    add utility functions for text2token and vice versa, generate text using GPTModel
    note that I still get 1 token output only instead of 5 more (max_token_size=10)
    this is similar to generate_simple_text in llm-infra and needs to be debugged.
    (am I missing a loop for the text concat with previous input?)

--- pretraining/textgenerate.py
+++ pretraining/textgenerate.py
@@ -14,10 +14,36 @@ GPT_CONFIG_124M = {
     "drop_rate": 0.1,
     "qkv_bias": False
 }
+
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
 model.eval()
 print(model)
 
+# add utility functions for text to tokenID conversion
 
+import tiktoken
+from generate_text_simple import generate_text_simple
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0) # unsqueeze(0) adds the batch dimension
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+start_context = "Every effort moves you"
+tokenizer = tiktoken.get_encoding("gpt2")
+
+token_ids = generate_text_simple(
+    model = model,
+    idx = text_to_token_ids(start_context, tokenizer),
+    max_new_tokens = 10,
+    context_size = GPT_CONFIG_124M["context_length"]
+)
+
+print("token ids: ", token_ids)
+print("Output text: \n", token_ids_to_text(token_ids, tokenizer))
 
