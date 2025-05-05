6d39db37fbf73928b746e7654fb9b88fd90b02a6
Anupam <anupam.kaul@yahoo.com>
2025-05-05 09:29:56 -0700

    Start work on applying loss function on an actual dataset with training and validation distributions

---
+++ pretraining/loss-training-validation.py
@@ -0,0 +1,20 @@
+'''
+Lets now prep up the infra for writing up the loss function
+and dividing the data set into training and validation sizes
+
+previously in eval.. we looked at cross-entropy as the loss
+function (negative average log probability difference between target and current iteration)
+
+We apply the loss calculation to the entire dataset
+
+The dataset size:
+1. Start with "The Verdict"
+2. Explore Project Gutenberg
+3. Scale with Llama like LLMs (7B parameter), and go to 175B, 1T etc
+'''
+
+
+
+
+
+
