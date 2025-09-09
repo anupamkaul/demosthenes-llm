916a071614aff4904cc798b95f7ba339e2745194
Anupam <anupam.kaul@yahoo.com>
2025-09-09 13:20:24 -0700

    Add inference code and a log
    Log shows a lot of things need to be fixed. Don't need the arch, don't
    need an actual eval of a pre-built string. The following chat (not a loop
    yet) shows that "the verdict" output prints gibberish (since its been
    over-trained on one story only). But at least I have a way to check
    inference independent of the model.

---
+++ pretraining/inference.py
@@ -0,0 +1,62 @@
+'''
+Use this as sample inference code to the demosthenes model
+'''
+
+import torch
+
+import tiktoken
+tokenizer = tiktoken.get_encoding("gpt2")
+
+import textgenerate
+from textgenerate import text_to_token_ids, generate_text_simple, token_ids_to_text
+
+# load previously saved instance of the model (to check inference) 
+#(start with the simple model generated from a single file (the verdict))
+
+from GPTModel import GPTModel
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 256,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12, 
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+model = GPTModel(GPT_CONFIG_124M)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+try:
+    model.load_state_dict(torch.load("./model/model.pth", map_location=device))
+    print("\nmodel loaded .. <enter>")
+    input()
+except FileNotFoundError:
+    print("model not found on disk. monitor as a one time thing, error out if repeats")
+    exit
+
+token_ids = generate_text_simple(
+    model          = model,
+    idx            = text_to_token_ids("Every effort moves you", tokenizer), # or the user's input (for a chat)
+    max_new_tokens = 25,
+    context_size   = GPT_CONFIG_124M["context_length"]     
+
+)
+
+print("\nchat output: ", token_ids_to_text(token_ids, tokenizer))
+
+# now we ask user for their input and check how we do
+user_input = input("chat with me: (and press enter) ")
+
+token_ids = generate_text_simple(
+    model          = model,
+    idx            = text_to_token_ids(user_input, tokenizer), # or the user's input (for a chat)
+    max_new_tokens = 25,
+    context_size   = GPT_CONFIG_124M["context_length"]     
+
+)
+
+print("\nchat output: ", token_ids_to_text(token_ids, tokenizer))
+
+
