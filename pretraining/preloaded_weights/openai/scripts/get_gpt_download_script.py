5539f153f7fb63ac476dfcf8a223c3a7406a43e5
Anupam <anupam.kaul@yahoo.com>
2025-09-09 16:52:57 -0700

    Add infra for preloading weights for demosthenes llm

---
+++ pretraining/preloaded_weights/openai/scripts/get_gpt_download_script.py
@@ -0,0 +1,13 @@
+'''
+code to get the script that will fetch open source weights
+from a specific repo (e.g. openAI)
+'''
+import urllib.request
+url = (
+    "https://raw.githubusercontent.com/rasbt/"
+    "LLMs-from-scratch/main/ch05/"
+    "01_main-chapter-code/gpt_download.py"
+)
+filename = url.split('/')[-1]
+urllib.request.urlretrieve(url, filename)
+
