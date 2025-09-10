29e3e3192f9b640b116a5183e571beed87a20a23
Anupam <anupam.kaul@yahoo.com>
2025-09-09 17:09:24 -0700

    Start inspecting settings and params from the download checkpoints (weights) Realized that I need to create a generate function that also uses top_k sampling and temperature scaling (for these preloaded weights) so I might as well first add those bells and whistles to the verdict (and gutenberg) models and experiment a bit before returning to the preloaded weights workstream...

--- pretraining/preloaded_weights/openai/scripts/exec_gpt_download.py
+++ pretraining/preloaded_weights/openai/scripts/exec_gpt_download.py
@@ -3,3 +3,8 @@ from gpt_download import download_and_load_gpt2
 settings, params = download_and_load_gpt2(
     model_size="124M", models_dir="gpt2"
 )
+
+# lets inspect settings and parameter dictionary keys
+
+print("Settings: ", settings)
+print("Params: ", params.keys())
