fd3b397f2a7460dd5c74e3dd004d74fb8ef7eb06
Anupam <anupam.kaul@yahoo.com>
2025-10-08 15:21:57 -0700

    Add DataLoaders to create batches of 8 training samples in a batch. Data loader loads such batched data for training, validation and test samples. These mini batches each include 8 training samples + their label data of 0/1 classification (ham/spam)
    In total we create 130 training batches +19 validation batches +38 test batches
    Next, will prepare the arch for the actual training model

--- finetune-classification/spam-dataloader.py
+++ finetune-classification/spam-dataloader.py
@@ -38,10 +38,79 @@ val_dataset = SpamDataset(
     max_length=train_dataset.max_length,
     tokenizer=tokenizer
 )
+print(val_dataset.max_length)
+
 test_dataset = SpamDataset(
     csv_file="test.csv",
     max_length=train_dataset.max_length,
     tokenizer=tokenizer
 )
+print(test_dataset.max_length)
+
+# now that the classes are instantiated and the reformatting (padding) of the 
+# 3 datasets has happened, we now create the training, validation and test
+# data Loaders that load the text messages in batches of size 8
+
+from torch.utils.data import DataLoader
+
+num_workers = 0
+batch_size = 8
+
+import torch
+torch.manual_seed(123)
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=num_workers,
+    drop_last=True,
+)
+val_loader = DataLoader(
+    dataset=val_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False,
+)
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False,
+)
+
+# to test the above code and check that batches of expected size are created,
+# we can iterate over the training_loader and print the tensor dimensions of 
+# the last batch (note that iterating will involve a for loop with a NOP)
+
+for input_batch, target_batch in train_loader:
+    pass
+
+print("Input batch dimensions:", input_batch.shape)
+print("Label batch dimensions", target_batch.shape)
+
+'''
+Input batch dimensions: torch.Size([8, 120])
+Label batch dimensions torch.Size([8])
+
+As we can see, input batches (each batch) consists of 8 training
+samples of size 120 each. The label tensor stores the class labels (0, 1 here : ham, spam)
+to the 8 training samples (classifications for training)`
+
+Lets also check how many batches are there in each dataset (training, validation and test)
+
+'''
+
+print(f"{len(train_loader)} training batches")
+print(f"{len(val_loader)} validation batches")
+print(f"{len(test_loader)} test batches")
+
+'''
+130 training batches
+19 validation batches
+38 test batches
+'''
+
+# now that we have prepared the data, we need to prepare the model for fine tuning
 
 
