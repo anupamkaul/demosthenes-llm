f214a6805b08976cccce0cfe2eefba2f55f38d98
Anupam <anupam.kaul@yahoo.com>
2025-10-27 15:52:02 -0700

    add instruction dataset class. First with this CR, it formats entries to a specific prompt template, and next it tokenizes it, resulting in a sequence of tokenIDs that my model can process

--- finetune-instruction-follow/dataset_tuning.py
+++ finetune-instruction-follow/dataset_tuning.py
@@ -34,3 +34,49 @@ print("Training set length:", len(train_data))
 print("Test set length:", len(test_data))
 print("Validation set length:", len(val_data))
 
+'''
+Next, we will create training batches. 
+
+Previously the training batches were created automatically by the 
+PyTorch DataLoader class, which employs a default collate function 
+to combine lists of samples into batches. A collate function is 
+responsible for taking a list of individual data samples and merging them 
+into a single batch that can be processed efficiently by the model during training.
+
+However, the batching process for instruction fine-tuning is a bit more involved 
+and requires us to create our own custom collate function that we will later plug 
+into the DataLoader. We implement this custom collate function to handle the specific 
+requirements and formatting of our instruction fine-tuning dataset.
+
+(see images/custom-collation-training-batches.png)
+
+Let's do this in several steps. First, code an InstructionDataset class that 
+applies 'format_input" (stylize_prompts) and "pretokenizes" all inputs in the
+dataset, similar to SpamDataset. This is implemented in the __init__ constructor
+of the following class:
+
+'''
+
+import torch
+from torch.utils.data import Dataset
+from stylize_prompts import format_input
+
+class InstructionDataset(Dataset):
+
+    def __init__(self, data, tokenizer):
+
+        self.data = data
+        self.encoded_texts = []
+        for entry in data:
+            instruction_plus_input = format_input(entry)
+            response_text = f"\n\n### Response:\n{entry['output']}"
+            full_text = instruction_plus_input + response_text
+            self.encoded_texts.append(tokenizer.encode(full_text))
+
+    def __get_item__(self, index):
+        return self.encoded_texts[index]
+
+    def __len__(self):
+        return len(self.data)
+
+
