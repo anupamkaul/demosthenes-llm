4505d262bee51836e9d5092930b4a03d05bedd06
Anupam <anupam.kaul@yahoo.com>
2025-12-26 10:46:15 -0800

    Microbatch trained the 355M demosthenes pretrained model for instruction fine tuning. This worked on my asus ubuntu 22.04 because of the 11G buff/cache arch.
    Results are better with Epoch1 than with Epoch2. Model has been locally
    generated. So I am able to microbatch the 124M model for IF on my dell
    laptop and the 355M model for IF on my asus laptop, and can proceed
    further now.

--- finetune-instruction-follow/model.py
+++ finetune-instruction-follow/model.py
@@ -42,8 +42,8 @@ model_configs = {
     "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
 }
 
-#CHOOSE_MODEL = "gpt2-medium (355M)"
-CHOOSE_MODEL = "gpt2-small (124M)"
+CHOOSE_MODEL = "gpt2-medium (355M)"
+#CHOOSE_MODEL = "gpt2-small (124M)"
 BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
 
 model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
